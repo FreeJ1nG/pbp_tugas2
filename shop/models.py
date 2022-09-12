4d0dd085bc9e5764d8811f4a3bf6ec1a657da252
Andrew Jeremy <Andrewjeremy12345@gmail.com>
2022-09-12 10:52:43 +0700

    reset db migrations for production

--- shop/models.py
+++ shop/models.py
@@ -5,7 +5,7 @@ import uuid
 
 # Create your models here.
 class Item(models.Model):
-  uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  uuid = models.UUIDField(primary_key=True, default=uuid.uuid4)
   name = models.CharField(max_length=50)
   price = models.IntegerField()
   description = models.TextField()
